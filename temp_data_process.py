6ed077e55cda8cf827b636c477de438bfbf44d87
luozhilin <luozhilin99@qq.com>
2022-12-10 16:29:05 +0800

    add process query and argparser

--- temp_data_process.py
+++ temp_data_process.py
@@ -1,6 +1,6 @@
 import os
 import jsonlines
-
+import argparse
 
 def process_table(table):
     text = ' '.join(table['header'])
@@ -9,12 +9,9 @@ def process_table(table):
     return text
 
 
-def process_corpus(dataset):
-    if not os.path.exists('data'):
-        os.mkdir('data')
-
-    dest_f = jsonlines.open('data/corpus.jsonl', 'w')
-    for table in jsonlines.open(f'datasets/{dataset}/tables.jsonl'):
+def process_corpus(dataset, input_dir, output_dir):
+    dest_f = jsonlines.open(os.path.join(output_dir, 'corpus.jsonl'), 'w')
+    for table in jsonlines.open(os.path.join(input_dir, dataset, 'tables.jsonl')):
         dest_f.write({
             'docid': table['id'],
             'title': table['title'],
@@ -22,10 +19,30 @@ def process_corpus(dataset):
         })
 
 
-def main():
-    dataset = "WTQ"
-    process_corpus(dataset)
+def process_query(dataset, input_dir, output_dir):
+    for split in ['train', 'dev', 'test']:
+        dest_f = jsonlines.open(os.path.join(output_dir, f'{split}.jsonl'), 'w')
+        for qa in jsonlines.open(os.path.join(input_dir, dataset, f'{split}.jsonl')):
+            dest_f.write({
+                'query_id': qa['id'],
+                'query': qa['question']
+            })
+
+
+def main(args):
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    process_corpus(args.dataset, args.input_dir, args.output_dir)
+    process_query(args.dataset, args.input_dir, args.output_dir)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--input_dir', type=str, default="datasets")
+    parser.add_argument('--output_dir', type=str, default="data")
+
+    args = parser.parse_args()
+
+    main(args)
