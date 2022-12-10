152392a5b16e81d0e3fbc3281746e01267974911
luozhilin <luozhilin99@qq.com>
2022-12-10 19:34:54 +0800

    fix datasets

--- datasets/process_data_for_eval.py
+++ datasets/process_data_for_eval.py
@@ -2,6 +2,7 @@ import os
 import jsonlines
 import argparse
 
+
 def process_table(table):
     text = ' '.join(table['header'])
     for row in table['rows']:
@@ -9,9 +10,9 @@ def process_table(table):
     return text
 
 
-def process_corpus(dataset, input_dir, output_dir):
-    dest_f = jsonlines.open(os.path.join(output_dir, 'corpus.jsonl'), 'w')
-    for table in jsonlines.open(os.path.join(input_dir, dataset, 'tables.jsonl')):
+def process_corpus(dataset,):
+    dest_f = jsonlines.open('corpus.jsonl', 'w')
+    for table in jsonlines.open(os.path.join(dataset, 'tables.jsonl')):
         dest_f.write({
             'docid': table['id'],
             'title': table['title'],
@@ -19,10 +20,10 @@ def process_corpus(dataset, input_dir, output_dir):
         })
 
 
-def process_query(dataset, input_dir, output_dir):
+def process_query(dataset):
     for split in ['train', 'dev', 'test']:
-        dest_f = jsonlines.open(os.path.join(output_dir, f'{split}.jsonl'), 'w')
-        for qa in jsonlines.open(os.path.join(input_dir, dataset, f'{split}.jsonl')):
+        dest_f = jsonlines.open(f'{split}.jsonl', 'w')
+        for qa in jsonlines.open(os.path.join(dataset, f'{split}.jsonl')):
             dest_f.write({
                 'query_id': qa['id'],
                 'query': qa['question']
@@ -33,15 +34,13 @@ def main(args):
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
 
-    process_corpus(args.dataset, args.input_dir, args.output_dir)
-    process_query(args.dataset, args.input_dir, args.output_dir)
+    process_corpus(args.dataset)
+    process_query(args.dataset)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, required=True)
-    parser.add_argument('--input_dir', type=str, default="datasets")
-    parser.add_argument('--output_dir', type=str, default="data")
 
     args = parser.parse_args()
 
