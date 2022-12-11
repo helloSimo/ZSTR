0f15f96aac2bc145b1e12c9439cfdcad5a95288f
luozhilin <luozhilin99@qq.com>
2022-12-11 17:16:30 +0800

    using table_processer

--- datasets/process_data_for_eval.py
+++ datasets/process_data_for_eval.py
@@ -1,22 +1,25 @@
 import os
 import jsonlines
 import argparse
+from transformers import AutoTokenizer
+from ..table_utils.table_processor import get_processor
 
 
-def process_table(table):
-    text = ' '.join(table['header'])
-    for row in table['rows']:
-        text += ' ' + ' '.join(row)
-    return text
+def process_corpus(dataset, max_cell_length, delimiter, tokenizer_name_or_path):
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
+    processer = get_processor(max_cell_length=max_cell_length,
+                              max_input_length=512,
+                              tokenizer=tokenizer,
+                              include_title=False,
+                              index_row=delimiter,
+                              row_choose=True)
 
-
-def process_corpus(dataset,):
     dest_f = jsonlines.open('corpus.jsonl', 'w')
     for table in jsonlines.open(os.path.join(dataset, 'tables.jsonl')):
         dest_f.write({
             'docid': table['id'],
             'title': table['title'],
-            'text': process_table(table)
+            'text': processer.process_table(table)
         })
 
 
@@ -34,13 +37,16 @@ def main(args):
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
 
-    process_corpus(args.dataset)
+    process_corpus(args.dataset, args.max_cell_length, args.delimiter, args.tokenizer_name_or_path)
     process_query(args.dataset)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--dataset', type=str, default='WTQ', choices=['WTQ', 'WikiSQL', 'NQTables'])
+    parser.add_argument('--max_cell_length', type=int, default=8)
+    parser.add_argument('--delimiter', action='store_true')
+    parser.add_argument('--tokenizer_name_or_path', type=str, default='bert-base-uncased')
 
     args = parser.parse_args()
 
